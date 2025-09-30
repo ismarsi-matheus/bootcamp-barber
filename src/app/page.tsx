0cbbb3d2a7723dd3ab7e3074e77cdbab6fa1d5c7
ismarsi-matheus <matheusleo.senac@gmail.com>
2025-09-30 16:18:42 -0300

    feat(barbershop): add BarbershopItem component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -67,7 +67,15 @@ const Home = async () => {
         <h2 className="mb-3 mt-3 text-xs font-bold uppercase text-gray-400">
           Recomendados
         </h2>
-        <div className="flex gap-4">
+        <div className="flex gap-4 overflow-auto [&::-webkit-scrollbar]:hidden">
+          {barbershops.map((barbershop) => (
+            <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+          ))}
+        </div>
+        <h2 className="mb-3 mt-3 text-xs font-bold uppercase text-gray-400">
+          Populares
+        </h2>
+        <div className="flex gap-4 overflow-auto [&::-webkit-scrollbar]:hidden">
           {barbershops.map((barbershop) => (
             <BarbershopItem key={barbershop.id} barbershop={barbershop} />
           ))}
