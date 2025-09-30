f266d0308c948e1f4670aafeb515ed09e64d3ffb
ismarsi-matheus <matheusleo.senac@gmail.com>
2025-09-30 00:50:30 -0300

    chore: config shadcn components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Header from "./_components/header"
 import { Button } from "./_components/ui/button"
 import { SearchIcon } from "lucide-react"
 import Image from "next/image"
+import { Card, CardContent } from "./_components/ui/card"
 
 const Home = () => {
   return (
@@ -12,14 +13,14 @@ const Home = () => {
       <div className="p-5">
         <h2 className="text-xl font-bold">Olá ,Mahtheus</h2>
         <p>Segunda-feira, 05 de agosto.</p>
-
+        {/* search */}
         <div className="mt-6 flex items-center gap-2">
           <Input placeholder="Faça a sua busca..." />
           <Button>
             <SearchIcon />
           </Button>
         </div>
-
+        {/* banner */}
         <div className="mt-06 relative h-[150px] w-full">
           <Image
             alt="Banner"
@@ -28,6 +29,11 @@ const Home = () => {
             className="rounded-xl object-cover"
           />
         </div>
+
+        {/* Agendamento*/}
+        <Card className="mt-6">
+          <CardContent></CardContent>
+        </Card>
       </div>
     </div>
   )
