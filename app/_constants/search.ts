93ee2b50fa2583c1ae706e36fb2de1f889eb3d38
ismarsi-matheus <matheusleo.senac@gmail.com>
2025-10-07 02:43:46 -0300

    feat: add Bookingitem component

---
+++ app/_constants/search.ts
@@ -0,0 +1,31 @@
+interface QuickSearchOption {
+  imageUrl: string;
+  title: string;
+}
+
+export const quickSearchOptions: QuickSearchOption[] = [
+  {
+    imageUrl: "/cabelo.svg",
+    title: "Cabelo",
+  },
+  {
+    imageUrl: "/barba.svg",
+    title: "barba",
+  },
+  {
+    imageUrl: "/acabamento.svg",
+    title: "Acabamneto",
+  },
+  {
+    imageUrl: "/massagem.svg",
+    title: "Massagem",
+  },
+  {
+    imageUrl: "/sobrancelha.svg",
+    title: "sobrancelha",
+  },
+  {
+    imageUrl: "/hidratacao.svg",
+    title: "Hidratacao",
+  },
+];
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,12 @@
-import { FootprintsIcon, SearchIcon } from "lucide-react";
+import { SearchIcon } from "lucide-react";
 import Header from "./_components/header";
 import { Button } from "./_components/ui/button";
 import { Input } from "./_components/ui/input";
 import Image from "next/image";
 import { Card, CardContent } from "./_components/ui/card";
-import { Badge } from "./_components/ui/badge";
-import { Avatar } from "./_components/ui/avatar";
-import { AvatarImage } from "@radix-ui/react-avatar";
 import { db } from "./_lib/prisma";
 import BarbershopItem from "./_components/barbershop-item";
+import { quickSearchOptions } from "./_constants/search";
 
 const Home = async () => {
   const barbershops = await db.barbershop.findMany({});
@@ -32,34 +30,17 @@ const Home = async () => {
         </div>
 
         <div className="mt-6 flex gap-3 overflow-x-scroll [&::-webkit-scrollbar]:hidden">
-          <Button className="gap-2" variant="secondary">
-            <Image src="/cabelo.svg" width={16} height={18} alt="Cabelo" />
-            Cabelo
-          </Button>
-          <Button className="gap-2" variant="secondary">
-            <Image src="/barba.svg" width={16} height={18} alt="Cabelo" />
-            Barba
-          </Button>
-          <Button className="gap-2" variant="secondary">
-            <Image src="/acabamento.svg" width={16} height={18} alt="Cabelo" />
-            Acabamento
-          </Button>
-          <Button className="gap-2" variant="secondary">
-            <FootprintsIcon size={16} />
-            Pezinho
-          </Button>
-          <Button className="gap-2" variant="secondary">
-            <Image src="/sobrancelha.svg" width={16} height={18} alt="Cabelo" />
-            Sobrancelha
-          </Button>
-          <Button className="gap-2" variant="secondary">
-            <Image src="/hidratacao.svg" width={16} height={18} alt="Cabelo" />
-            Hidratacao
-          </Button>
-          <Button className="gap-2" variant="secondary">
-            <Image src="/massagem.svg" width={16} height={18} alt="Cabelo" />
-            Massagem
-          </Button>
+          {quickSearchOptions.map((option) => (
+            <Button key={option.title} className="gap-2" variant="secondary">
+              <Image
+                src={option.imageUrl}
+                width={16}
+                height={18}
+                alt={option.title}
+              />
+              {option.title}
+            </Button>
+          ))}
         </div>
 
         <div className="relative mt-6 h-[150px] w-full">
@@ -70,31 +51,6 @@ const Home = async () => {
             alt="BANNER FSW"
           />
         </div>
-
-        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
-          Agendamentos
-        </h2>
-
-        <Card className="mt-6">
-          <CardContent className="flex justify-between p-0">
-            <div className="flex flex-col gap-2 py-5 pl-5">
-              <Badge className="w-fit">Confirmado</Badge>
-              <h3>Corte de Cabelo</h3>
-              <div className="flex items-center gap-2">
-                <Avatar className="h-6 w-6">
-                  <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png" />
-                </Avatar>
-                <p className="text-sm">Barbearia FSW</p>
-              </div>
-            </div>
-
-            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
-              <p className="text-sm">Agosto</p>
-              <p className="text-2xl">05</p>
-              <p className="text-sm">20:00</p>
-            </div>
-          </CardContent>
-        </Card>
 
         <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
           Recomendados
